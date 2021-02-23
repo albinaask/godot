7f10dda1172759c78db80734b1ddd32b0cea3ddb
Albin <albin.a.ask@gmail.com>
2021-02-23 20:18:25 +0100

    boilerplate code

---
+++ modules/xpbd/xpbd_physics_server.h
@@ -0,0 +1,13 @@
+#ifndef XPBD_PHYSICS_SERVER_H
+#define XPBD_PHYSICS_SERVER_H
+
+#include "core/rid.h"
+#include "servers/physics_server_3d.h"
+
+class XPBDPhysicsServer3D : public PhysicsServer3D {
+	GDCLASS(XPBDPhysicsServer3D, PhysicsServer3D);
+};
+
+
+
+#endif
