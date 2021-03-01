2999dcbc57c78e6d0dfa5b58212faab17fe398b3
Albin <albin.a.ask@gmail.com>
2021-03-01 18:31:07 +0100

    WTF???

--- modules/xpbd/register_types.cpp
+++ modules/xpbd/register_types.cpp
@@ -1,2 +1,33 @@
 #include "register_types.h"
 
+#include "core/class_db.h"
+//#include "core/project_settings.h"
+
+/**
+	@author albinaask
+*/
+
+#ifndef _3D_DISABLED
+PhysicsServer3D *_createXPBDPhysicsCallback() {
+	return memnew(XPBDPhysicsServer3D);
+}
+#endif
+
+void register_xpbd_types() {
+#ifndef _3D_DISABLED
+	PhysicsServer3DManager::register_server("XPBD", &_createXPBDPhysicsCallback);
+
+	//PhysicsServer3DManager::set_default_server("Bullet", 1);
+	//GLOBAL_DEF("physics/3d/active_soft_world", true);
+	//ProjectSettings::get_singleton()->set_custom_property_info("physics/3d/active_soft_world", PropertyInfo(Variant::BOOL, "physics/3d/active_soft_world"));
+#endif
+}
+
+void unregister_xpbd_types() {
+}
+
+
+
+
+
+
---
+++ modules/xpbd/register_types.h
@@ -0,0 +1,1 @@
+#ifndef REGISTER_XPBD_TYPES_H #define REGISTER_XPBD_TYPES_H  /** 	@author albinaask */  void register_xpbd_types(); void unregister_xpbd_types();  #endif 
