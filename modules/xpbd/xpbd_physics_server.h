2742dc61e45927b0e7c735fa38735dc2f680492c
Albin <albin.a.ask@gmail.com>
2021-03-17 15:12:42 +0100

    physics server

--- modules/xpbd/xpbd_physics_server.h
+++ modules/xpbd/xpbd_physics_server.h
@@ -1,13 +1,15 @@
 #ifndef XPBD_PHYSICS_SERVER_H
 #define XPBD_PHYSICS_SERVER_H
 
-#include "core/rid.h"
+#include "core/templates/rid.h"
 #include "servers/physics_server_3d.h"
 
 class XPBDPhysicsServer3D : public PhysicsServer3D {
 	GDCLASS(XPBDPhysicsServer3D, PhysicsServer3D);
 };
 
-
+public:
+	BulletPhysicsServer3D();
+	~BulletPhysicsServer3D();
 
 #endif
